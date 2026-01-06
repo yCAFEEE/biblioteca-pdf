2b38db242249649796191bb65990a38036c78935
yCAFEEE <yuridan2705@gmail.com>
2026-01-06 20:46:45 -0300

    add backend funcional

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,25 +5,29 @@ export default function Home(){
     const [pdfs, setPdfs] = useState([]);
 
     useEffect(() => {
-        const pdfFiles = [
-            "../../pdfDir/IRODORI A1 Introdutório.pdf",
-            "../../pdfDir/Bass Solo Segredos da Improvisação - Nico Assumpção.pdf",
-            "../../pdfDir/Sistemas Operacionais Modernos (Andrew S. Tanenbaum, Herbert Bos).pdf"
-        ];
-
-        setPdfs(pdfFiles);
-    });
+        fetch("http://localhost:3000/pdfs")
+        .then(r => r.json())
+        .then(setPdfs)
+        .catch(err => {
+            console.error("Falha ao buscar PDFs", err);
+        });
+    }, []);
 
     return(
         <main>
             <div className="content-container">
+                <h1>PDFs</h1>
+                {pdfs.length === 0 ? (
+                    <p>Nenhum arquivo PDF encontrado no caminho: ./client/public/pdfDir/</p>
+                ) : (
                 <ul>
                     {pdfs.map((pdf, idx) => (
                         <li key={idx}>
-                            <a href={`/pdfs/${pdf}`}>{pdf}</a>
+                            <a href={`http://localhost:3000${pdf.url}`}>{pdf.name}</a>
                         </li>
                     ))}
                 </ul>
+                )}
             </div>
         </main>
     );
