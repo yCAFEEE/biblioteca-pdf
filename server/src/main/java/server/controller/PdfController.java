ec46dcfb424210c294c7a4e130f0a03806d7e064
yCAFEEE <yuridan2705@gmail.com>
2026-07-09 21:16:46 -0300

    add PdfController.java com função para retornar a lista de pdfs da pasta pdfDir/

---
+++ server/src/main/java/server/controller/PdfController.java
@@ -0,0 +1,43 @@
+package server.controller;
+
+import java.io.File;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class PdfController{
+    private final String pdfsDir = "../client/public/pdfDir";
+
+    @GetMapping("/pdfs")
+    public ResponseEntity<?> listPdfs(){
+        try{
+            File folder = new File(pdfsDir);
+            File[] files = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".pdf"));
+            List<Map<String, String>> list = new ArrayList<>();
+
+            if(files != null){
+                for(File file : files){
+                    String name = file.getName();
+                    String encodedName = URLEncoder.encode(name, StandardCharsets.UTF_8).replace("+", "%20");
+
+                    Map<String, String> pdfInfo = new HashMap<>();
+                    pdfInfo.put("name", name);
+                    pdfInfo.put("url", "/pdf-files/" + encodedName);
+
+                    list.add(pdfInfo);
+                }
+            }
+            return ResponseEntity.ok(list);
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Erro ao listar os arquivos PDFs"));
+        }
+    }
+    
+}
