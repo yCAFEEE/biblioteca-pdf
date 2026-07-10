2a7907671b6f22108b200abe62219ebb45a71bf6
yCAFEEE <yuridan2705@gmail.com>
2026-07-09 21:56:05 -0300

    usar caminho dinâmico para evitar erro 404 ao acessar um pdf

--- server/src/main/java/server/config/WebConfig.java
+++ server/src/main/java/server/config/WebConfig.java
@@ -1,5 +1,7 @@
 package server.config;
 
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -22,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer{
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/pdfs-files/**")
-            .addResourceLocations("file:../client/public/pdfDir/");
+        String localPath = Paths.get("..", "client", "public", "pdfDir").toAbsolutePath().normalize().toUri().toString();
+        registry.addResourceHandler("/pdfs-files/**").addResourceLocations(localPath);
     }
 }
